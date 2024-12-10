9db77eb8431024cd29017644bd9b6d59bb21c1ea
Mohammed Tohamy <125455529+mohammedtohamym@users.noreply.github.com>
2024-12-10 16:58:15 +0200

    Initial app setup with routes added

---
+++ lib/home/homescreen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Homescreen extends StatelessWidget {
+  static const String routeName = 'homescreen_route';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:islami_app/home/homescreen.dart';
+
+void main(){
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      initialRoute: Homescreen.routeName,
+      routes: {
+        Homescreen.routeName : (context) => Homescreen(),
+      },
+    );
+  }
+}
+
